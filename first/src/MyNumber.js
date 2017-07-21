cc747ba31edcf09c6d3af77d9058cdb0dd5ea9d5
Peerapong Thongpubet <pt1988@gmail.com>
2017-07-21 11:30:05 +0700

    add first project

---
+++ first/src/MyNumber.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class MyNumber extends Component{
+    render(){
+        return (
+            <div> MyNumber is 10!</div>
+        );
+
+    }
+}
+
+export default MyNumber;
